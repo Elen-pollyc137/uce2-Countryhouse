03b5b91e7985dd486d1b1940fdd918f5eb82b636
Poly <poliana_elen@hotmail.com>
2021-07-28 19:03:47 -0300

    refector: Type Next page component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,21 @@
 import styles from './styles.module.scss';
 import Link from 'next/link';
+import { NextPage } from 'next';
 
-export function Header() {
+const Header: NextPage = () => {
   return (
     <header className={styles.headerContainer}>
       <nav className={styles.headerContent}>
-        <h1>Country house</h1>
+        <Link href="/location">
+          <a className={styles.logo}>Country house</a>
+        </Link>
+
         <Link href="/">
-          <a>Login</a>
+          <a className={styles.login}>Login</a>
         </Link>
       </nav>
     </header>
   );
-}
+};
+
+export default Header;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/global.scss';
 import { AppProps } from 'next/app';
-import { Header } from '../components/Header';
+import  Header  from '../components/Header';
 import { AuthProvider } from '../contexts/AuthContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
