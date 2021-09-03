96ec33fbab7d89a991d7adfb63ff78b9e55d56a2
Heberth Junor <hjunor@gmail.com>
2021-09-03 14:46:18 -0300

    add: enveriomets is defined vercel plataform api

--- src/pages/api/[file].ts
+++ src/pages/api/[file].ts
@@ -24,7 +24,6 @@ type IFileQuery = {
 const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
-    // Only allow requests with GET, POST and OPTIONS
     methods: ['GET', 'POST', 'OPTIONS', 'PUT'],
   })
 );
